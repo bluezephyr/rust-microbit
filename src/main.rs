5ee0fe6a4c6eef114e559c9e78dfc7feb4eac5a1
Blue Zephyr <blue.zephyr.git@gmail.com>
2024-09-27 22:09:52 +0200

    Blink a LED using the nrf52833 PAC

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,21 @@
 
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
+use nrf52833_pac::{p0::pin_cnf::W, Peripherals};
 use panic_halt as _;
 
 #[entry]
 fn main() -> ! {
-    let mut x: usize = 0;
+    let p: Peripherals = Peripherals::take().unwrap();
+    p.P0.pin_cnf[21].write(|w: &mut W| w.dir().output());
+    p.P0.pin_cnf[28].write(|w: &mut W| w.dir().output());
+
+    let mut is_on: bool = false;
     loop {
-        x += 1;
-        for _ in 0..x {
+        p.P0.out.write(|w| w.pin21().bit(is_on));
+        for _ in 0..200_000 {
             nop();
         }
+        is_on = !is_on;
     }
 }
